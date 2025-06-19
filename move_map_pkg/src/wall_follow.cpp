ba7e606a2cd8be32eb4163ea8918ffcfc7914850
morsoth <94766863+morsoth@users.noreply.github.com>
2025-06-19 17:14:40 +0200

    Update wall_follow.cpp

--- move_map_pkg/src/wall_follow.cpp
+++ move_map_pkg/src/wall_follow.cpp
@@ -13,33 +13,41 @@ public:
     void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
         geometry_msgs::Twist move;
 
+        // Lidar distances
         float front_right_dist = getSafeRange(msg, 330);
         float right_front_dist = getSafeRange(msg, 300);
         float right_dist = getSafeRange(msg, 270);
 
+        // Angle between right_front_dist and right_dist
         float angle = (300 - 270) * (M_PI / 180);
 
+        // Thresholds
         float front_thresh = 0.25;
         float wall_thresh = 0.3;
 
+        // Offset to correct errors
         float offset = 0.01;
-        
+
+        // Turn right if there is no wall in right (rare case)
         if (right_dist > wall_thresh) {
-            ROS_INFO("derecha - wall_dist >>");
+            ROS_INFO("right - wall_dist >>");
             move.linear.x = 0.05;
             move.angular.z = -0.22;
         }
+        // Turn left if there is wall in front and right
         else if (front_right_dist < front_thresh) {
-            ROS_INFO("izquierda - front_dist <<");
+            ROS_INFO("left - front_dist <<");
             move.angular.z = 0.5;
         }
+        // Turn left if getting too close to the wall
         else if (right_front_dist < right_dist/cos(angle) + offset) {
-            ROS_INFO("izquierda");
+            ROS_INFO("left");
             move.linear.x = 0.05;
             move.angular.z = 0.28;
         }
+        // Turn right if drifting away from the wall
         else if (right_front_dist > right_dist/cos(angle) + offset) {
-            ROS_INFO("derecha");
+            ROS_INFO("right");
             move.linear.x = 0.05;
             move.angular.z = -0.28;
         }
@@ -52,6 +60,7 @@ private:
     ros::Subscriber sub_;
     ros::Publisher pub_;
 
+    // Get distance from a lidar angle
     float getSafeRange(const sensor_msgs::LaserScan::ConstPtr& msg, int index) {
         float range = msg->ranges[index];
 
